1f5fa21b4e990a2dc5159f8a9970b6ffb72aaef5
nayisntaround87 <naylasalwahayati@gmail.com>
2024-03-26 14:48:22 +0700

    menambah int penjumlahan

--- PertemuanKeempat/PertemuanKeempat.cpp
+++ PertemuanKeempat/PertemuanKeempat.cpp
@@ -14,6 +14,34 @@ void inputData()
     cin >> bilangan2;
 }
 
+int penjumlahan(int a, int b)
+{
+    return a + b;
+}
+
+int main()
+{
+    int pilihan;
+
+    do
+    {
+        cout << "Kalkulator sederhana" << endl;
+        cout << "====================" << endl;
+        cout << "1. penjumlahan" << endl;
+        cout << "2. pengurangan" << endl;
+        cout << "3. perkalian" << endl;
+        cout << "4. pembagian" << endl; 
+        cout 
+    }
+}
+
+
+
+
+
+
+
+
 int main()
 {
     std::cout << "Hello World!\n";
