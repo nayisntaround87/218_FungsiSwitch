d3177f79863574158d95f78c4778fb2f39501c61
nayisntaround87 <naylasalwahayati@gmail.com>
2024-03-26 14:35:57 +0700

    input data

--- PertemuanKeempat/PertemuanKeempat.cpp
+++ PertemuanKeempat/PertemuanKeempat.cpp
@@ -4,6 +4,15 @@
 #include <iostream>
 using namespace std;
 
+int bilangan1, bilangan2;
+
+void inputData()
+{
+    cout << "Masukan bilangan pertama : ";
+    cin >> bilangan1;
+    cout << "Masukan bilangan kedua : ";
+    cin >> bilangan2;
+}
 
 int main()
 {
